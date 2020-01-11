baeadb36bf1a4d76b00e23ab6d485b383383c7e7
Kevin Wang <kwangsan@gmail.com>
2020-01-10 20:24:51 -0500

    Postlink

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,14 @@
 import { SEO } from "../components/SEO"
 import { Layout } from "../components/Layout"
+import Link from "next/link"
+
+const PostLink = props => (
+  <li>
+    <Link href={`/post?title=${props.title}`}>
+      <a>{props.title}</a>
+    </Link>
+  </li>
+)
 
 function Home() {
   return (
@@ -7,6 +16,11 @@ function Home() {
       <SEO title="Home" />
       <Layout>
         <h1>Home</h1>
+        <ul>
+          <PostLink title="Hello Next.js" />
+          <PostLink title="Learn Next.js is awesome" />
+          <PostLink title="Deploy apps with Zeit" />
+        </ul>
       </Layout>
     </>
   )
