9fba99b87824c4aa5bd630bb9666bf8cb19685b3
Kevin Wang <kwangsan@gmail.com>
2020-01-12 18:39:19 -0500

    more progress!

--- src/pages/heatmap/index.tsx
+++ src/pages/heatmap/index.tsx
@@ -9,16 +9,34 @@ import { SEO } from "../../components/SEO"
 interface Bin {
   value: number
 }
+type Week = [Bin, Bin, Bin, Bin, Bin, Bin, Bin]
 interface Props {
-  data?: Bin[]
+  data?: Week[]
 }
 
 const HEIGHT = 10
-const Svg = styled(animated.svg)`
-  border: 1px dashed grey;
-  width: 400px;
-  height: 200px;
+const MARGIN = 3
+
+const HeatmapContainer = styled(animated.div)`
+  display: flex;
+  width: 100%;
 `
+const Svg = styled(animated.svg)`
+  padding: 1rem;
+  border: 1px solid lightgrey;
+  border-radius: 0.2rem;
+  margin-left: auto;
+  margin-right: auto;
+  display: flex;
+  justify-content: flex-end;
+  /* width: ${_.ceil(365 / 7) * 13}px; */
+  width: 100%;
+  overflow-x: scroll;
+`
+const myColor = d3
+  .scaleLinear<string, string>()
+  .domain([1, 10])
+  .range(["#ebedf0", "#196127"])
 
 export default function Heatmap({ data }: Props) {
   const d3Container = useRef(null)
@@ -26,35 +44,31 @@ export default function Heatmap({ data }: Props) {
   useEffect(() => {
     if (data && d3Container.current) {
       const svg = d3.select(d3Container.current)
-      var myColor = d3
-        .scaleLinear<string, string>()
-        .domain([1, 10])
-        .range(["black", "#69b3a2"])
 
-      // Bind D3 data
-      const update = svg
-        .append("g")
-        .selectAll()
+      const g = svg
+        .selectAll("g")
         .data(data)
+        .enter()
+        .append("g")
+        /** translate X of the columns, based on the week */
+        .attr(
+          "transform",
+          (d: Week, i) => `translate(${i * (HEIGHT + MARGIN)},0)`
+        )
 
-      // Enter new D3 elements
-      update
+      const rect = g
+        .selectAll("rect")
+        /** past nested data to <rect> children */
+        .data((d: Week) => d)
         .enter()
         .append("rect")
-        .attr("x", (d, i) => i * (HEIGHT + 1))
-        // .attr("y", HEIGHT)
         .attr("width", HEIGHT)
         .attr("height", HEIGHT)
-        .attr("margin", 5)
-        .style("font-size", 24)
-        .style("fill", (d: Bin) => myColor(d.value))
-        .text((d: Bin) => d.value)
-
-      // Update existing D3 elements
-      update.attr("x", (d, i) => i * HEIGHT).text((d: Bin) => d.value)
-
-      // Remove old D3 elements
-      update.exit().remove()
+        /** offset Y, based on the day */
+        .attr("y", (d, i) => {
+          return i * (HEIGHT + MARGIN)
+        })
+        .style("fill", d => myColor(d.value))
     }
   }, [data, d3Container.current])
   // prettier-ignore
@@ -63,30 +77,18 @@ export default function Heatmap({ data }: Props) {
       <SEO title="Heatmap" />
 
       <h1>Heatmap</h1>
-      <div style={{ border: `1px dotted grey` }}>
+      <HeatmapContainer>
         <Svg className="d3-component" ref={d3Container} />
-      </div>
-    
-      <svg>
-        <g transform="translate(0, 0)">
-          <rect className="day" width="10" height="10" x="14" y="0" fill="#c6e48b" data-count="6" data-date="2019-01-13" />
-          <rect className="day" width="10" height="10" x="14" y="13" fill="#c6e48b" data-count="4" data-date="2019-01-14"/>
-          <rect className="day" width="10" height="10" x="14" y="26" fill="#c6e48b" data-count="2" data-date="2019-01-15"/>
-          <rect className="day" width="10" height="10" x="14" y="39" fill="#c6e48b" data-count="4" data-date="2019-01-16"/>
-          <rect className="day" width="10" height="10" x="14" y="52" fill="#c6e48b" data-count="7" data-date="2019-01-17"/>
-          <rect className="day" width="10" height="10" x="14" y="65" fill="#c6e48b" data-count="9" data-date="2019-01-18"/>
-          <rect className="day" width="10" height="10" x="14" y="78" fill="#c6e48b" data-count="7" data-date="2019-01-19"/>
-        </g>
-      </svg>
+      </HeatmapContainer>
     </>
   )
 }
 
-Heatmap.getInitialProps = async ({ req }): Promise<{ data: Bin[] }> => {
+Heatmap.getInitialProps = async ({ req }): Promise<{ data: Week[] }> => {
   const data = Array(365)
-    .fill("")
+    .fill(null)
     .map(() => ({ value: Math.floor(Math.random() * 10) }))
   return {
-    data,
+    data: _.chunk(data, 7) as Week[],
   }
 }
