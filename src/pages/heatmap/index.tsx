e73f79bf48c632ad9bb599f005be36fbb5790a7c
Kevin Wang <kwangsan@gmail.com>
2020-01-13 01:48:02 -0500

    revive heatmap page for dev easiness

---
+++ src/pages/heatmap/index.tsx
@@ -0,0 +1,31 @@
+import _ from "lodash"
+import ms from "ms"
+
+import Heatmap, { Week } from "../../components/Heatmap"
+
+export default function HeatmapPage({ data }) {
+  return (
+    <>
+      <Heatmap data={data} />
+    </>
+  )
+}
+
+HeatmapPage.getInitialProps = async ({ req }): Promise<{ data: Week[] }> => {
+  const data = Array(365) // [0...364]
+    .fill(null)
+    .map((e, i) => {
+      const time = new Date().getTime() - ms(`${364 - i} days`)
+      const month = new Date(time).getMonth() + 1
+      const date = new Date(time).getDate()
+      const year = new Date(time).getFullYear()
+
+      return {
+        value: Math.floor(Math.random() * 100),
+        date: `${month}-${date}-${year}`,
+      }
+    })
+  return {
+    data: _.chunk(data, 7) as Week[],
+  }
+}
