09d197ddd88c79ec221a683f07a5da5ba2bc2608
Kevin Wang <kwangsan@gmail.com>
2020-01-17 08:22:10 -0500

    add api/heatmap for data fetching with SWR

---
+++ src/pages/api/heatmap/index.ts
@@ -0,0 +1,96 @@
+import gql from "graphql-tag"
+import { NextApiRequest, NextApiResponse } from "next"
+import ms from "ms"
+import _ from "lodash"
+
+import { client } from "../../../apolloClient"
+
+const GET_ALL_SESSIONS = gql`
+  query GetAllSessions {
+    getAllSessions {
+      id
+      created
+      attempts {
+        id
+        grade
+        send
+      }
+    }
+  }
+`
+interface Data {
+  getAllSessions: {
+    // [Session]
+    id: string
+    created: Date
+    attempts: {
+      // [Attempt]
+      id: string
+      grade: string
+      send: boolean
+    }[]
+  }[]
+}
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  const { count } = req.query
+  const SELECT = parseInt(count)
+  const masterList = Array(SELECT) // [0...364]
+    .fill(null)
+    .map((e, i) => {
+      const timestamp = new Date(
+        new Date().getTime() - ms(`${SELECT - 1 - i} days`)
+      )
+      const month = timestamp.getUTCMonth()
+      const date = timestamp.getUTCDate()
+      const year = timestamp.getUTCFullYear()
+
+      return { date: new Date(year, month, date) }
+    })
+
+  try {
+    const queryResult = await client.query<Data>({
+      query: GET_ALL_SESSIONS,
+    })
+
+    const {
+      data: { getAllSessions: data },
+    } = queryResult
+
+    /**
+     * @WARN
+     * indices.length must equal data.length
+     * DONOT _.compact out the falsy values.
+     */
+    const indices: any[] = _.flow(
+      _.partialRight(_.map, e => {
+        const timestamp = new Date(e.created)
+        const month = timestamp.getUTCMonth()
+        const date = timestamp.getUTCDate()
+        const year = timestamp.getUTCFullYear()
+
+        if (e.attempts.length > 0) {
+          return +new Date(year, month, date)
+        }
+      })
+      // _.compact
+    )(data)
+
+    const heatmapData = _.map(masterList, e => {
+      const index = indices.indexOf(+e.date)
+      return {
+        date: new Date(e.date),
+        attempts: index > -1 ? data[index].attempts : [],
+      }
+    })
+    // console.log(heatmapData)
+
+    const chunked = _.chunk(heatmapData, 7)
+    res.status(200).json({
+      heatmapData: chunked,
+    })
+  } catch (error) {
+    console.log(error)
+    res.status(500).send(error)
+  }
+}
