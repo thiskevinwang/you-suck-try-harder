6bbcc288394a6bbd52f96217437ca3ab5488d924
Kevin Wang <kwangsan@gmail.com>
2020-02-09 14:24:37 -0500

    remove sessions; refactor heatmap data logic

--- src/pages/user/[id].tsx
+++ src/pages/user/[id].tsx
@@ -1,7 +1,6 @@
 import { useRouter } from "next/router"
 import useSWR from "swr"
 import { ApolloQueryResult } from "apollo-client"
-import gql from "graphql-tag"
 import _ from "lodash"
 
 import { Layout } from "../../components/Layout"
@@ -59,97 +58,3 @@ export default function UserPage() {
     </Layout>
   )
 }
-
-const GET_ALL_SESSIONS = gql`
-  query GetAllSessions {
-    getAllSessions {
-      id
-      created
-      attempts {
-        id
-        grade
-        send
-      }
-    }
-  }
-`
-interface Data {
-  getAllSessions: {
-    // [Session]
-    id: string
-    created: Date
-    attempts: {
-      // [Attempt]
-      id: string
-      grade: string
-      send: boolean
-    }[]
-  }[]
-}
-
-/**
- * @deprecated Move heatmap data fetching into useSWR.
- * SSR doesn't need this data, so fetch it clientside.
- */
-
-// const SELECT = 365
-// UserPage.getInitialProps = async ({ req }) => {
-//   const masterList = Array(SELECT) // [0...364]
-//     .fill(null)
-//     .map((e, i) => {
-//       const timestamp = new Date(
-//         new Date().getTime() - ms(`${SELECT - 1 - i} days`)
-//       )
-//       const month = timestamp.getUTCMonth()
-//       const date = timestamp.getUTCDate()
-//       const year = timestamp.getUTCFullYear()
-
-//       return { date: new Date(year, month, date) }
-//     })
-
-//   try {
-//     const queryResult = await client.query<Data>({
-//       query: GET_ALL_SESSIONS,
-//     })
-
-//     const {
-//       data: { getAllSessions: data },
-//     } = queryResult
-
-//     /**
-//      * @WARN
-//      * indices.length must equal data.length
-//      * DONOT _.compact out the falsy values.
-//      */
-//     const indices: any[] = _.flow(
-//       _.partialRight(_.map, e => {
-//         const timestamp = new Date(e.created)
-//         const month = timestamp.getUTCMonth()
-//         const date = timestamp.getUTCDate()
-//         const year = timestamp.getUTCFullYear()
-
-//         if (e.attempts.length > 0) {
-//           return +new Date(year, month, date)
-//         }
-//       })
-//       // _.compact
-//     )(data)
-
-//     const heatmapData = _.map(masterList, e => {
-//       const index = indices.indexOf(+e.date)
-//       return {
-//         date: new Date(e.date),
-//         attempts: index > -1 ? data[index].attempts : [],
-//       }
-//     })
-//     // console.log(heatmapData)
-
-//     const chunked = _.chunk(heatmapData, 7)
-//     return {
-//       heatmapData: chunked,
-//     }
-//   } catch (error) {
-//     console.log(error)
-//     return
-//   }
-// }
