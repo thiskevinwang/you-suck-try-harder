15fd710d558773ba285ce73eae913e8d61fe469b
Kevin Wang <kwangsan@gmail.com>
2020-02-14 21:33:59 -0500

    install @apollo/client

--- src/apolloClient.ts
+++ src/apolloClient.ts
@@ -1,4 +1,4 @@
-import { ApolloClient } from "apollo-client"
+import { ApolloClient } from "@apollo/client"
 import { InMemoryCache } from "apollo-cache-inmemory"
 import { HttpLink } from "apollo-link-http"
 import { setContext } from "apollo-link-context"
