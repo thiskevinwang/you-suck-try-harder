0981887f94ab89daec4397fbd3cae9d34daea656
Kevin Wang <kwangsan@gmail.com>
2020-02-16 17:56:47 -0500

    extract UserDetails from index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,53 +1,20 @@
 import Link from "next/link"
 import useSWR from "swr"
 import { ApolloQueryResult } from "apollo-client"
-import styled from "styled-components"
 
-import { SEO } from "../components/SEO"
-import { Layout } from "../components/Layout"
-
+import { SEO } from "components/SEO"
+import { Layout } from "components/Layout"
+import { UserDetails } from "components/User/Details"
 import { UserDetailsLoader } from "components/Loaders/UserDetailsLoader"
 
 function fetcher(url: string) {
   return fetch(url).then(r => r.json())
 }
 
-const StyledUserDetails = styled.div`
-  margin-top: 1rem;
-  margin-bottom: 1rem;
-
-  display: flex;
-  align-items: center;
-  img {
-    margin-left: 1rem;
-    margin-right: 1rem;
-    width: 3rem;
-    height: 3rem;
-    border-radius: 50%;
-  }
-  div > h3 {
-    min-height: 1rem;
-    min-width: 8rem;
-    margin: 0px 0px 1pt;
-  }
-  div > p {
-    min-height: 1rem;
-    min-width: 5rem;
-    margin: 0px;
-  }
-`
 const UserDetailsLink = ({ id, user }) => (
   <Link href="/user/[id]" as={`/user/${id}`}>
     <a>
-      <StyledUserDetails>
-        <img src={user.avatar_url} />
-        <div>
-          <h3>{user.username}</h3>
-          <p>
-            {user.first_name} {user.last_name}
-          </p>
-        </div>
-      </StyledUserDetails>
+      <UserDetails user={user} />
     </a>
   </Link>
 )
