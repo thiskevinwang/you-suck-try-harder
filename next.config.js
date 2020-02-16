0035116b165f60a5517894c70ac8a02eac61597e
Kevin Wang <kwangsan@gmail.com>
2020-02-16 09:41:00 -0500

    add src/hooks to next.config.js

--- next.config.js
+++ next.config.js
@@ -7,6 +7,7 @@ module.exports = {
     config.resolve.alias["components"] = path.join(__dirname, "src/components")
     config.resolve.alias["consts"] = path.join(__dirname, "src/consts")
     config.resolve.alias["state"] = path.join(__dirname, "src/state")
+    config.resolve.alias["hooks"] = path.join(__dirname, "src/hooks")
     config.resolve.alias["apolloClient"] = path.join(
       __dirname,
       "src/apolloClient.ts"
