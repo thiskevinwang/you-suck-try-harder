8709f8b148727cbbd748511f022c28fa1b0b621d
Kevin Wang <kwangsan@gmail.com>
2020-02-19 01:51:30 -0500

    useAuthentication: { currentUserId: string }

--- src/hooks/useAuthentication/index.ts
+++ src/hooks/useAuthentication/index.ts
@@ -7,19 +7,19 @@ import { Strings } from "consts/Strings"
  * - Checks local storage for `token`
  * - Adds `onstorage` event listener to update state accordingly
  *
- * @returns {object} `{ currentUserId }`
+ * @returns {object} `{ currentUserId: string }`
  */
-export function useAuthentication() {
+export function useAuthentication(): { currentUserId: string } {
   const token =
     typeof window !== "undefined" && localStorage.getItem(Strings.token)
-  const [currentUserId, setCurrentUserId] = useState<number | null>()
+  const [currentUserId, setCurrentUserId] = useState<string | null>()
   useDebugValue(currentUserId)
 
   useEffect(() => {
     jwt.verify(token, process.env.GATSBY_APP_SECRET, (err, decoded: any) => {
       const userId = decoded?.userId
       if (userId) {
-        setCurrentUserId(userId)
+        setCurrentUserId(`${userId}`)
       }
     })
 
