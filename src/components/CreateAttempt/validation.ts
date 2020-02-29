953b5f2f457dde6e5d5e66148d1055093fba5ffc
Kevin Wang <kwangsan@gmail.com>
2020-02-29 15:17:14 -0500

    fix long yup error message with `typeError()`

--- src/components/CreateAttempt/validation.ts
+++ src/components/CreateAttempt/validation.ts
@@ -3,7 +3,10 @@ import * as Yup from "yup"
 export const ValidationSchema = Yup.object().shape({
   attempts: Yup.array().of(
     Yup.object().shape({
-      grade: Yup.number().required("required"),
+      grade: Yup.number()
+        /** https://github.com/jquense/yup/issues/211 */
+        .typeError("you must specify a number")
+        .required("required"),
       send: Yup.boolean(),
       flash: Yup.boolean(),
     })
