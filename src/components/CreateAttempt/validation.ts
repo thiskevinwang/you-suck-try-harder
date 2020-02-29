fead59263ae9cf3895a5bfb126a14ff5e9de8d3a
Kevin Wang <kwangsan@gmail.com>
2020-02-29 15:21:21 -0500

    validation min(1)

--- src/components/CreateAttempt/validation.ts
+++ src/components/CreateAttempt/validation.ts
@@ -1,14 +1,16 @@
 import * as Yup from "yup"
 
 export const ValidationSchema = Yup.object().shape({
-  attempts: Yup.array().of(
-    Yup.object().shape({
-      grade: Yup.number()
-        /** https://github.com/jquense/yup/issues/211 */
-        .typeError("you must specify a number")
-        .required("required"),
-      send: Yup.boolean(),
-      flash: Yup.boolean(),
-    })
-  ),
+  attempts: Yup.array()
+    .of(
+      Yup.object().shape({
+        grade: Yup.number()
+          /** https://github.com/jquense/yup/issues/211 */
+          .typeError("you must specify a number")
+          .required("required"),
+        send: Yup.boolean(),
+        flash: Yup.boolean(),
+      })
+    )
+    .min(1),
 })
