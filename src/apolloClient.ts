34f211c0881b71b60b66f4e3a0f5bbeeba33fb70
Kevin Wang <kwangsan@gmail.com>
2020-02-29 21:17:44 -0500

    export extra apolloClient for API routes

--- src/apolloClient.ts
+++ src/apolloClient.ts
@@ -26,22 +26,25 @@ const httpLink = new HttpLink({
   fetch: !isBrowser && fetch,
 })
 
-// export const client = new ApolloClient({
-//   connectToDevTools: isBrowser,
-//   ssrMode: !isBrowser, // Disables forceFetch on the server (so queries are only run once)
-//   // Provide required constructor fields
-//   cache: cache,
-//   link: authLink.concat(httpLink as any) as any,
-//   // Provide some optional constructor fields
-//   name: "you suck try harder",
-//   version: "0.0.0.alpha-1",
-//   queryDeduplication: false,
-//   defaultOptions: {
-//     watchQuery: {
-//       fetchPolicy: "cache-and-network",
-//     },
-//   },
-// })
+/**
+ * export this 'extra client' to keep API routes functional
+ */
+export const client = new ApolloClient({
+  connectToDevTools: isBrowser,
+  ssrMode: !isBrowser, // Disables forceFetch on the server (so queries are only run once)
+  // Provide required constructor fields
+  cache: cache,
+  link: authLink.concat(httpLink as any) as any,
+  // Provide some optional constructor fields
+  name: "you suck try harder",
+  version: "0.0.0.alpha-1",
+  queryDeduplication: false,
+  defaultOptions: {
+    watchQuery: {
+      fetchPolicy: "cache-and-network",
+    },
+  },
+})
 
 export default function createApolloClient(initialState, ctx) {
   // The `ctx` (NextPageContext) will only be present on the server.
