8a4d6d1a4763135cd17ce483cecdc7579b6b9912
Kevin Wang <kwangsan@gmail.com>
2020-02-29 20:19:45 -0500

    add lib path to config

--- next.config.js
+++ next.config.js
@@ -6,6 +6,7 @@ module.exports = {
     config.resolve.alias["src"] = path.join(__dirname, "src")
     config.resolve.alias["components"] = path.join(__dirname, "src/components")
     config.resolve.alias["icons"] = path.join(__dirname, "src/icons")
+    config.resolve.alias["lib"] = path.join(__dirname, "src/lib")
     config.resolve.alias["consts"] = path.join(__dirname, "src/consts")
     config.resolve.alias["state"] = path.join(__dirname, "src/state")
     config.resolve.alias["hooks"] = path.join(__dirname, "src/hooks")
