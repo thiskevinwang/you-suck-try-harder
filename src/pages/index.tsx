d76b84f0294425192a244374afb0d8d97ea85949
Kevin Wang <kwangsan@gmail.com>
2020-02-29 21:02:17 -0500

    stop using useSwr ☹️

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,11 @@
 import Link from "next/link"
-import useSWR from "swr"
-import { ApolloQueryResult } from "apollo-client"
+import { useQuery, gql } from "@apollo/client"
 
 import { SEO } from "components/SEO"
 import { Layout } from "components/Layout"
 import { UserDetails } from "components/User/Details"
 import { UserDetailsLoader } from "components/Loaders/UserDetailsLoader"
-
 import { useAuthentication } from "hooks/useAuthentication"
-
-function fetcher(url: string) {
-  return fetch(url).then(r => r.json())
-}
 
 const UserDetailsLink = ({ id, user, isActive }) => (
   <Link href="/user/[id]" as={`/user/${id}`}>
@@ -28,20 +22,28 @@ interface Users {
   }[]
 }
 
+const GET_ALL_USERS_QUERY = gql`
+  query GetAllUsers {
+    users: getAllUsers {
+      id
+      username
+      avatar_url
+      first_name
+      last_name
+    }
+  }
+`
+
 function Overview() {
-  const { data, error } = useSWR<ApolloQueryResult<Users>>(
-    `/api/users`,
-    fetcher
-  )
+  const { data } = useQuery<Users>(GET_ALL_USERS_QUERY)
   const { currentUserId } = useAuthentication()
-  console.log("---", typeof data?.data?.users?.[0]?.id, typeof currentUserId)
   return (
     <>
       <SEO title="Overview" />
       <Layout>
         <h1>Overview</h1>
         <ul>
-          {data?.data?.users.map(user => (
+          {data?.users.map(user => (
             <UserDetailsLink
               id={user.id}
               key={`${user.id}-${user.username}`}
