8d13ecdec7df3534ec822ff498a94814ed16343d
Kevin Wang <kwangsan@gmail.com>
2020-03-02 00:53:12 -0500

    validation - require send&flash

--- src/components/CreateAttempt/validation.ts
+++ src/components/CreateAttempt/validation.ts
@@ -8,9 +8,11 @@ export const ValidationSchema = Yup.object().shape({
           /** https://github.com/jquense/yup/issues/211 */
           .typeError("you must specify a number")
           .required("required"),
-        send: Yup.boolean(),
-        flash: Yup.boolean(),
+        send: Yup.boolean().required(),
+        flash: Yup.boolean().required(),
       })
     )
-    .min(1),
+    // don't know where these validation errors show up yet
+    .min(1, "Please log at least one")
+    .max(5, "For now, just submit 5 at a time please"),
 })
