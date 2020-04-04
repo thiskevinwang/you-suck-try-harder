5802d8d8fc9be96b8d612cef813ed7d2997241ae
Kevin Wang <kwangsan@gmail.com>
2020-04-04 11:40:56 -0400

    redo LeftSidebar

--- src/components/LeftSidebar/index.tsx
+++ src/components/LeftSidebar/index.tsx
@@ -1,24 +1,19 @@
 import * as React from "react"
 import Link from "next/link"
 import { useRouter } from "next/router"
-import PropTypes from "prop-types"
-import { useSelector, useDispatch } from "react-redux"
+import { useSelector } from "react-redux"
 import { useMediaQuery } from "@material-ui/core"
 import styled, { BaseProps } from "styled-components"
 import { animated, useSpring } from "react-spring"
 
-import { RootState, setIsNavOpen } from "state"
+import { RootState } from "state"
 import { useAuthentication } from "hooks/useAuthentication"
 import { Breakpoints } from "consts/Breakpoints"
 import { Strings } from "consts/Strings"
-import { Hamburger } from "components/Hamburger"
 
 import { LogOut, LogIn, Home, UserPlus } from "icons"
 
 const LeftSidebar = () => {
-  const dispatch = useDispatch()
-  const toggleIsNavOpen = () => dispatch(setIsNavOpen(!isNavOpen))
-
   const { currentUserId } = useAuthentication()
   const router = useRouter()
   const handleLogout = () => {
@@ -27,7 +22,7 @@ const LeftSidebar = () => {
   }
 
   const isNavOpen = useSelector((s: RootState) => s.isNavOpen)
-  const lgUp = useMediaQuery(Breakpoints.lgUp)
+  const lgUp = useMediaQuery(Breakpoints.lgUp, { noSsr: true })
   const props = useSpring({
     transform: lgUp
       ? `translateX(0rem)`
@@ -35,12 +30,12 @@ const LeftSidebar = () => {
       ? `translateX(16rem)`
       : `translateX(0rem)`,
   })
+  const leftSidebarWrapperProps = useSpring({
+    marginLeft: lgUp ? `0rem` : `-16rem`,
+  })
   return (
-    <LeftSidebarWrapper>
+    <LeftSidebarWrapper style={leftSidebarWrapperProps}>
       <LeftSidebarNav style={props}>
-        <LeftSidebarHeader>
-          <Hamburger isOpen={isNavOpen} clickHandler={toggleIsNavOpen} />
-        </LeftSidebarHeader>
         <StyledNavItem>
           <Link href="/">
             <a>
@@ -97,18 +92,6 @@ const StyledNavItemInner = styled.div`
     margin-right: 1rem;
     margin-left: 1rem;
   }
-
-  /* color: ${(p: BaseProps) => p.theme.colors.borderColor}; */
-  svg:not(.filled) {
-    /* stroke: ${(p: BaseProps) => p.theme.colors.borderColor}; */
-  }
-  :hover,
-  :focus {
-    /* color: ${(p: BaseProps) => p.theme.colors.main}; */
-    svg:not(.filled) {
-      /* stroke: ${(p: BaseProps) => p.theme.colors.main}; */
-    }
-  }
 `
 
 const StyledNavItem = styled.li`
@@ -125,43 +108,32 @@ const StyledNavItem = styled.li`
   }
 `
 
-const LeftSidebarHeader = styled.header`
-  height: ${(p: BaseProps) => p.theme.headerHeight};
-  padding: 1rem 1rem;
-  border-bottom: 1px solid ${(p: BaseProps) => p.theme.colors.borderColor};
-  /* hide the sidebar header on lgUp */
-  @media ${Breakpoints.lgUp} {
-    display: none;
-  }
-`
-
-const LeftSidebarWrapper = styled.aside`
+const LeftSidebarWrapper = styled(animated.aside)`
   z-index: 1;
-  margin-left: -16rem;
   flex: 0 0 16rem;
-  /* font-size: 0.875rem; */
+
   @media ${Breakpoints.lgUp} {
-    margin-left: 0;
   }
 `
 
-const LeftSidebarNav = styled(animated.nav)`
+interface LeftSidebarNavProps {
+  navOpen?: boolean
+}
+const LeftSidebarNav = styled(animated.nav)<LeftSidebarNavProps>`
   background: ${(p: BaseProps) => p.theme.colors.leftSidebarNavBackground};
+  border-top: 1px solid ${(p: BaseProps) => p.theme.colors.borderColor};
   border-right: 1px solid ${(p: BaseProps) => p.theme.colors.borderColor};
   position: fixed;
-  top: 0;
+  top: calc(
+    ${(p: BaseProps) => p.theme.headerHeight} + 2 * 16px
+  ); /* match header height */
   bottom: 0;
   overflow-x: hidden;
   overflow-y: auto;
   width: 16rem;
   height: 100%;
-  /* padding: 1rem 0; */
   @media ${Breakpoints.lgUp} {
-    padding: 6.6rem 0 1rem;
   }
 `
-LeftSidebar.propTypes = {
-  navOpen: PropTypes.bool,
-}
 
 export default React.memo(LeftSidebar)
